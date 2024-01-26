3633cdba8cede7588980a5ec4b49c29ffff51cfc
xoo2001 <ekora2001@gmail.com>
2024-01-26 20:53:46 +0700

    X6739: Assert OTA

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,6 +21,9 @@ AB_OTA_PARTITIONS += \
     vbmeta_system
 BOARD_USES_RECOVERY_AS_BOOT := true
 
+# Assert
+TARGET_OTA_ASSERT_DEVICE := X6739
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
