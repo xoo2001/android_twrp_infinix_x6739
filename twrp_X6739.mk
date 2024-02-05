5706c1d9f32ce28d4007055b17cbfb68bf42b9c4
xoo2001 <ekora2001@gmail.com>
2024-02-05 23:05:59 +0700

    X6739: remove duplicate scripts

--- twrp_X6739.mk
+++ twrp_X6739.mk
@@ -16,7 +16,6 @@ $(call inherit-product, device/infinix/X6739/device.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
